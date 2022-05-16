52989587141c84cfca55d08590840ab270f1c34c
floxydio <diooktar@gmail.com>
2022-05-16 10:35:18 +0700

    add upload with multer

--- controllers/Product.js
+++ controllers/Product.js
@@ -1,34 +1,54 @@
 const connection = require("../database/mysql")
+const multer = require("multer")
 
+const upload = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  }, filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const storage = multer({ storage: upload,limits:{fileSize: 50000}})
 
-exports.findProduct = function(req,res) {
-  connection.query("SELECT * FROM products", function(err, results) {
-    if(err) throw err;
+exports.findProduct = function (req, res) {
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
     res.json({
       data: results
     })
   })
 }
 
-exports.createProduct = function(req,res) {
-  connection.query("INSERT INTO products SET ?", req.body, function(err, results) {
-    if(err) throw err;
-    res.json({
-      message: "Successfully Create Product"
-    })
+exports.createProduct = function (req, res) {
+  storage.single("upload")(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(500).json({
+        message: err.message
+      })
+    } else if (err) {
+    }
+    else {
+      req.body.upload = req.file.originalname
+      connection.query("INSERT INTO products SET ?", req.body, function (err, results) {
+        if (err) throw err;
+        res.json({
+          message: "Successfully Create Product"
+        })
+      })
+    }
   })
 }
 
-exports.editProductByID =  function(req,res) {
-  connection.query("UPDATE products SET ? WHERE id = ?", [req.body, req.params.id], function(err, results) {
-    if(err) throw err;
+exports.editProductByID = function (req, res) {
+  connection.query("UPDATE products SET ? WHERE id = ?", [req.body, req.params.id], function (err, results) {
+    if (err) throw err;
     res.json("Successfully Update Product")
   })
 }
 
-exports.deleteProductById = function(req,res) {
-  connection.query("DELETE FROM products WHERE id = ?", req.params.id, function(err, results) {
-    if(err) throw err;
+exports.deleteProductById = function (req, res) {
+  connection.query("DELETE FROM products WHERE id = ?", req.params.id, function (err, results) {
+    if (err) throw err;
     res.json("Successfully Delete Product")
   })
 }
--- main/app.js
+++ main/app.js
@@ -5,16 +5,17 @@ const bodyparser = require("body-parser")
 const cors = require("cors")
 const product = require("../controllers/Product")
 
-  app.use(cors())
+const path = require('path')
 
+  app.use(cors())
 
   app.use(bodyparser.urlencoded({extended: false}))
 
-  app.get("/", (res) => res.send("Pala Kau Hello World!"))
+  app.use("/img", express.static(path.join(__dirname, '../uploads')))
+  app.get("/", (req,res) => res.send("Pala Kau Hello World!"))
   app.get("/product", product.findProduct)
   app.post("/product",product.createProduct)
   app.put("/product/:id", product.editProductByID)
   app.delete("/product/:id", product.deleteProductById)
-
 
 app.listen(port, () => console.log(`Running on ${port}`))
